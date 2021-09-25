8df0fbc93c4f0cae3a483cd49dcc3163493cc8da
shiv-santosh <shivsj9755@gmail.com>
2021-09-25 17:22:31 +0530

    heap sort

--- heap.py
+++ heap.py
@@ -1,3 +1,49 @@
+from sorting import is_sorted
+
+
+def heap_sort(array):
+    sorted_array = []
+    array = heap_bottom_up(array)
+    while len(array) > 1:
+        sorted_array.append(array[0])
+        array[0] = array[-1]
+        array = array[:-1]
+        array = heapify(array, 0)
+    return sorted_array
+
+
+def heap_bottom_up(array):
+    first_node_with_child = int((len(array) - 3) / 2)
+    for i in range(first_node_with_child, -1, -1):
+        array = heapify(array, i)
+    return array
+
+
+def heapify(array, n):
+    c1 = (2 * n) + 1
+    c2 = (2 * n) + 2
+
+    # No child
+    if c1 >= len(array) or c2 >= len(array):
+        return array
+    # One child
+    if c1 == len(array):
+        if array[c1] < array[n]:
+            array[c1], array[n] = array[n], array[c1]
+            return heapify(array, c1)
+        return array
+    # Node lesser than equal to children
+    if array[n] <= array[c1] and array[n] <= array[c2]:
+        return array
+    # One or both children are greater than node
+    if array[c2] <= array[c1]:
+        array[c2], array[n] = array[n], array[c2]
+        return heapify(array, c2)
+    # c1 is smallest
+    array[c1], array[n] = array[n], array[c1]
+    return heapify(array, c1)
+
+
 def is_heap(array):
     i = 0
     while i < len(array):
@@ -11,5 +57,7 @@ def is_heap(array):
 
 
 if __name__ == '__main__':
-    array = [12, 543, 123, 6565, 876, 123, 5454, 6, 451234, 6565, 234, 4357867, 345, 345]
+    array = [9, 5, 7, 2, 8, 3, 11, 19, 32, 1, 92, 45, 2, 5, 9, 11, 3]
     print(is_heap(array))
+    print(is_heap(heap_bottom_up(array)))
+    print(is_sorted(heap_sort(array)))
